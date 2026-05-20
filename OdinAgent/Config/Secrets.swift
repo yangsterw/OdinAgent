77baf2d007ab347aaa2d8af3d55818551169e37c
Yang Wang <ywang132@mgh.harvard.edu>
2026-05-20 01:24:23 -0400

    update gitignore

---
+++ OdinAgent/Config/Secrets.swift
@@ -0,0 +1,34 @@
+//
+//  Secrets.swift
+//  OdinAgent
+//
+//  Created by yang on 5/20/26.
+//
+
+
+enum Secrets {
+
+    static let trelloAPIKey =
+        "YOUR_API_KEY"
+
+    static let trelloToken =
+        "YOUR_TOKEN"
+
+    static let trelloBoardID =
+        "YOUR_BOARD_ID"
+
+    static let trelloListToday =
+        "YOUR_LIST_ID"
+
+    static let trelloListInProgress =
+        "YOUR_LIST_ID"
+
+    static let trelloListUpcoming =
+        "YOUR_LIST_ID"
+
+    static let trelloListFuture =
+        "YOUR_LIST_ID"
+
+    static let trelloListBlocked =
+        "YOUR_LIST_ID"
+}
